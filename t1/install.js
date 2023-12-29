81a4b912d83b3cbd28a874d067d3cc48c11e6a77
Shashi Kumar <contact9508914855@gmail.com>
2023-12-29 20:46:29 +0530

    Update install.js

--- t1/install.js
+++ t1/install.js
@@ -1,24 +1,40 @@
+let displayCount = 0;
+const maxDisplayCount = 3; // Set a maximum number of displays
 
-// Show the install button if the app is installable
+// Check if the app is installable
 if (deferredPrompt) {
-    document.getElementById('installButton').style.display = 'block';
-  
-    // Handle the install button click
-    document.getElementById('installButton').addEventListener('click', () => {
-      // Show the install prompt
-      deferredPrompt.prompt();
-  
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-  
-        // Reset the deferredPrompt for the next time it's triggered
-        deferredPrompt = null;
-      });
+  const installButton = document.getElementById('installButton');
+
+  // Function to display the install button
+  const displayInstallButton = () => {
+    installButton.style.display = 'block';
+    displayCount++;
+  };
+
+  // Handle the install button click
+  installButton.addEventListener('click', () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      // Reset the display count and hide the install button if the user accepted
+      if (choiceResult.outcome === 'accepted') {
+        displayCount = 0;
+        installButton.style.display = 'none';
+      }
+
+      // Reset the deferredPrompt for the next time it's triggered
+      deferredPrompt = null;
     });
-  }
-  
+  });
+
+  // Display the install button up to maxDisplayCount times
+  const displayInterval = setInterval(() => {
+    if (displayCount < maxDisplayCount) {
+      displayInstallButton();
+    } else {
+      clearInterval(displayInterval);
+    }
+  }, 5000); // Adjust the interval as needed (e.g., every 5 seconds)
+}
