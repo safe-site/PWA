9722ff5383e89eae68fb839ca157c848967423f3
Shashi Kumar <contact9508914855@gmail.com>
2023-12-30 13:17:35 +0530

    h

--- t1/PWA/src/scripts/install-btn.js
+++ t1/PWA/src/scripts/install-btn.js
@@ -1,54 +1,52 @@
 let deferredPrompt; // Declare the variable
 let displayCount = 0;
-const maxDisplayCount = 3; // Set a maximum number of displays
+let appInstalled = false;
 
-// Check if the app is installable
-if (window.matchMedia('(display-mode: standalone)').matches) {
-  // The app is already installed
-} else {
-  // Function to display the install button
-  const displayInstallButton = () => {
-    const installButton = document.getElementById('installButton');
-    installButton.style.display = 'block';
-    displayCount++;
-  };
+// Function to display the install button
+const displayInstallButton = () => {
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+  displayCount++;
+  setTimeout(() => {
+    installButton.style.display = 'none';
+  }, 9000); // Display the button for 3 seconds
+};
 
-  // Event listener for the beforeinstallprompt event
-  window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent the default behavior to avoid showing the browser's install prompt
-    event.preventDefault();
+// Event listener for the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior to avoid showing the browser's install prompt
+  event.preventDefault();
 
-    // Store the event for later use
-    deferredPrompt = event;
+  // Store the event for later use
+  deferredPrompt = event;
 
-    // Display your custom install button
-    displayInstallButton();
-  });
+  // Display your custom install button
+  displayInstallButton();
+});
 
-  // Handle the install button click
-  document.getElementById('installButton').addEventListener('click', () => {
-    // Show the install prompt
-    deferredPrompt.prompt();
+// Handle the install button click
+document.getElementById('installButton').addEventListener('click', () => {
+  // Show the install prompt
+  deferredPrompt.prompt();
 
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      // Reset the display count and hide the install button if the user accepted
-      if (choiceResult.outcome === 'accepted') {
-        displayCount = 0;
-        document.getElementById('installButton').style.display = 'none';
-      }
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    // Reset the deferredPrompt for the next time it's triggered
+    deferredPrompt = null;
 
-      // Reset the deferredPrompt for the next time it's triggered
-      deferredPrompt = null;
-    });
-  });
-
-  // Display the install button up to maxDisplayCount times
-  const displayInterval = setInterval(() => {
-    if (displayCount < maxDisplayCount) {
-      displayInstallButton();
-    } else {
-      clearInterval(displayInterval);
+    // Check if the user accepted and set appInstalled to true
+    if (choiceResult.outcome === 'accepted') {
+      appInstalled = true;
+      document.getElementById('installButton').style.display = 'none';
     }
-  }, 5000); // Adjust the interval as needed (e.g., every 5 seconds)
-}
+  });
+});
+
+// Display the install button every 10 seconds until the app is installed
+const displayInterval = setInterval(() => {
+  if (!appInstalled) {
+    displayInstallButton();
+  } else {
+    clearInterval(displayInterval);
+  }
+}, 10000); // Display every 10 seconds
