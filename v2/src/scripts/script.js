f0d78bcc4c6092deee4937439601e17aea76a732
Shashi Kumar <contact9508914855@gmail.com>
2023-12-30 17:26:28 +0530

    n

--- v2/src/scripts/script.js
+++ v2/src/scripts/script.js
@@ -1,26 +1,3 @@
-function saveAndRedirect() {
-    var websiteUrl = document.getElementById('websiteInput').value;
-    if (isValidUrl(websiteUrl)) {
-        // Display loading spinner
-        document.getElementById('loadingSpinner').style.display = 'block';
-
-        // Save the URL to local storage
-        localStorage.setItem('savedWebsiteUrl', websiteUrl);
-
-        // Redirect after a short delay (simulating loading)
-        setTimeout(function() {
-            window.location.href = websiteUrl;
-        }, 1000);
-    } else {
-        alert('Please enter a valid URL');
-    }
-}
-
-function isValidUrl(url) {
-    var pattern = /^(http|https):\/\/[^ "]+$/;
-    return pattern.test(url);
-}
-
 window.onload = function () {
     var savedUrl = localStorage.getItem('savedWebsiteUrl');
     if (savedUrl) {
@@ -28,8 +5,11 @@ window.onload = function () {
         document.getElementById('loadingSpinner').style.display = 'block';
 
         // Redirect after a short delay (simulating loading)
-        setTimeout(function() {
+        setTimeout(function () {
             window.location.href = savedUrl;
-        }, 1000);
+        }, 0.001);
+    } else {
+        // Redirect to EnterURL.html if no URL is available
+        window.location.href = 'enter-url.html';
     }
 };
